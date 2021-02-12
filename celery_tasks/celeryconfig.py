bcb38aa4d8c476e444d5fe0e7ad21e94f4790bbd
RainrainWu <s0958334772@gmail.com>
2021-02-12 23:44:21 +0800

    feat(labs): add demo project for celery and flower (#2)

--- celery_tasks/celeryconfig.py
+++ celery_tasks/celeryconfig.py
@@ -1,5 +1,7 @@
-result_backend = "redis://localhost"
-broker_url = "redis://localhost:6379/0"
+import os
+
+result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost")
+broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
 
 task_serializer = "json"
 result_serializer = "json"
@@ -7,13 +9,17 @@ accept_content = ["json"]
 
 task_default_rate_limit = "10/s"
 result_backend_max_retries = "5"
+broker_transport_options = {
+    "queue_order_strategy": "priority",
+}
 
 task_annotations = {
     "labs.tasks.add_rate_limit": {"rate_limit": "1/s"},
 }
 
 task_routes = {
-    "labs.tasks.add_route_foo": {
-        "queue": "foo",
-    },
+    "labs.tasks.add_route_foo": {"queue": "foo",},
+    "labs.tasks.add_priority_high": {"queue": "priority", "priority": 0,},
+    "labs.tasks.add_priority_low": {"queue": "priority", "priority": 9,},
+    "labs.tasks.add_critical": {"queue": "critical",},
 }
